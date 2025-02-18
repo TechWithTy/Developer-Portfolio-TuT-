de62299f4f5e0600f977895691b0388bf7c8372a
Tyrique Daniel <tyrique1.daniel@gmail.com>
2025-02-17 18:36:35 -0700

    fixed dynamic links

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -7,11 +7,11 @@ export const personalData = {
   phone: "628-303-8011",
   address: "Denver, CO",
   github: "https://github.com/techwithty",
-  facebook: "https://www.facebook.com/", // No Facebook link in resume, kept placeholder
+  // facebook: "https://www.facebook.com/", // No Facebook link in resume, kept placeholder
   linkedIn: "https://www.linkedin.com/in/tyrique-daniel",
-  twitter: "https://twitter.com/", // No Twitter link in resume, kept placeholder
-  stackOverflow: "https://stackoverflow.com/", // No StackOverflow link in resume, kept placeholder
-  leetcode: "https://leetcode.com/", // No Leetcode link in resume, kept placeholder
+  // twitter: "https://twitter.com/", // No Twitter link in resume, kept placeholder
+  // stackOverflow: "https://stackoverflow.com/", // No StackOverflow link in resume, kept placeholder
+  // leetcode: "https://leetcode.com/", // No Leetcode link in resume, kept placeholder
   devUsername: "ben",
   resume: "https://www.canva.com/design/DAGOXaDGgeU/5RYzYxTqEjyIZfm24lZSlA/view?utm_content=DAGOXaDGgeU&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h0430b9c87e",
   calendly: "https://calendly.com/-tyrique/gtknme" // Added from resume
