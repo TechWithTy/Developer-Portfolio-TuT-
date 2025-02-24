4c970774cef1462d63224401fb43202622eafb1a
Tyrique Daniel <tyrique1.daniel@gmail.com>
2025-02-24 13:55:58 -0700

    added loading screen full

--- app/components/helper/loaders/cssLoading.jsx
+++ app/components/helper/loaders/cssLoading.jsx
@@ -1,9 +1,12 @@
 "use client";
 
-const CssLoadingScreen = ({ title = "Loading..." }) => {
+const CssLoadingScreen = ({ title = "Loading...", size = 48 }) => {
   return (
-    <div className="fixed inset-0 flex flex-col items-center justify-center bg-black text-white z-[99999]">
-      <div className="loader"></div>
+    <div className="fixed inset-0 flex flex-col items-center justify-center bg-black bg-opacity-80 text-white z-[99999]">
+      <div
+        className="loader"
+        style={{ width: `${size}px`, height: `${size}px` }}
+      ></div>
       <p className="mt-4 text-lg font-semibold">{title}</p>
     </div>
   );
