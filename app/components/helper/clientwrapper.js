2f77f2c08c13fc3f0a656315e7d43188fc84b650
Tyrique Daniel <tyrique1.daniel@gmail.com>
2025-02-24 12:35:32 -0700

    updated loading screen

--- app/components/helper/clientwrapper.js
+++ app/components/helper/clientwrapper.js
@@ -4,15 +4,15 @@ import LoadingScreen from "./loading";
 export default function ClientOnly({ children }) {
   const [isClient, setIsClient] = useState(false);
 
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     if (!isClient) {
-  //       setIsClient(true);
-  //     }
-  //   }, 300); // Rechecks every 300ms
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!isClient) {
+        setIsClient(true);
+      }
+    }, 300); // Rechecks every 300ms
 
-  //   return () => clearInterval(interval); // Cleanup interval on unmount
-  // }, [isClient]);
+    return () => clearInterval(interval); // Cleanup interval on unmount
+  }, [isClient]);
 
   if (!isClient) return <LoadingScreen />; // Show loading screen until true
 
--- app/components/helper/loading.jsx
+++ app/components/helper/loading.jsx
@@ -1,13 +1,18 @@
 "use client";
 import AnimationLottie from "./animation-lottie";
-import spaceLoading from '../../assets/lottie/space-loading.json'
+import spaceLoading from "../../assets/lottie/space-loading.json";
+
 const LoadingScreen = () => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black z-[9999]">
-      <AnimationLottie animationPath={spaceLoading} width="100px" />
+    <div
+      className="fixed inset-0 flex items-center justify-center bg-gradient-to-b from-[#10002b] via-[#3c096c] to-[#ff4d6d] z-[9999] 
+                 backdrop-blur-lg shadow-lg"
+    >
+      <div className="w-[150px] h-[150px] sm:w-[200px] sm:h-[200px]">
+        <AnimationLottie animationPath={spaceLoading} />
+      </div>
     </div>
   );
 };
 
 export default LoadingScreen;
-a
