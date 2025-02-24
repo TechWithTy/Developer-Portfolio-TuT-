81fc1d6be2b1f86d10f6f29b295924cbb48c7b7d
Tyrique Daniel <tyrique1.daniel@gmail.com>
2025-02-24 12:40:20 -0700

    updated lottie file

--- app/components/helper/loading.jsx
+++ app/components/helper/loading.jsx
@@ -1,15 +1,25 @@
 "use client";
+import { useEffect, useState } from "react";
 import AnimationLottie from "./animation-lottie";
-import spaceLoading from "../../assets/lottie/space-loading.json";
 
 const LoadingScreen = () => {
+  const [animationData, setAnimationData] = useState(null);
+
+  useEffect(() => {
+    import("../../assets/lottie/space-loading.json").then((data) =>
+      setAnimationData(data.default)
+    );
+  }, []);
+
+  if (!animationData) return null; // Prevent rendering before data loads
+
   return (
     <div
-      className="fixed inset-0 flex items-center justify-center bg-gradient-to-b from-[#10002b] via-[#3c096c] to-[#ff4d6d] z-[9999] 
-                 backdrop-blur-lg shadow-lg"
+      className="fixed inset-0 flex items-center justify-center bg-gradient-to-b from-[#10002b] via-[#3c096c] to-[#ff4d6d] 
+                 z-[9999] backdrop-blur-lg shadow-lg"
     >
       <div className="w-[150px] h-[150px] sm:w-[200px] sm:h-[200px]">
-        <AnimationLottie animationPath={spaceLoading} />
+        <AnimationLottie animationPath={animationData} />
       </div>
     </div>
   );
